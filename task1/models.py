82912dc3449fbb45ded4b3287a4eb551514b9cee
pincha100 <miha040222@gmail.com>
2025-01-07 01:04:05 +0700

    Initial commit

---
+++ task1/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)  # Имя покупателя
+    balance = models.DecimalField(max_digits=10, decimal_places=2)  # Баланс
+    age = models.IntegerField()  # Возраст
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)  # Название игры
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # Цена
+    size = models.DecimalField(max_digits=10, decimal_places=2)  # Размер файла
+    description = models.TextField()  # Описание
+    age_limited = models.BooleanField(default=False)  # Ограничение возраста 18+
+    buyers = models.ManyToManyField(Buyer, related_name='games')  # Покупатели игры
+
+    def __str__(self):
+        return self.title
+
