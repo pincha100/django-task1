f5b8394ba799d413cb564414db74d2a5f7b022cc
pincha100 <miha040222@gmail.com>
2025-01-13 01:18:30 +0700

    Сайт администрирования

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Game, Buyer
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    # Отображаемые поля в списке
+    list_display = ('title', 'cost', 'size')
+    # Фильтрация по полям
+    list_filter = ('size', 'cost')
+    # Поиск по полю title
+    search_fields = ('title',)
+    # Ограничение количества записей
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    # Отображаемые поля в списке
+    list_display = ('name', 'balance', 'age')
+    # Фильтрация по полям
+    list_filter = ('balance', 'age')
+    # Поиск по полю name
+    search_fields = ('name',)
+    # Ограничение количества записей
+    list_per_page = 30
+    # Только для чтения поле balance
+    readonly_fields = ('balance',)
+
